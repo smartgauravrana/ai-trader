4abad46776e988b058525fd525bb88c27d7b6079
Gaurav Rana <gaurav.ranaji96@gmail.com>
2024-04-04 12:52:37 +0530

    cron job updated

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -4,11 +4,13 @@ import { aggregateFundsData } from "./dashboard";
 
 // 9:10am IST
 cron.schedule("40 3 * * *", handleTokenRefresh);
+cron.schedule("50 3 * * *", handleTokenRefresh);
 cron.schedule("45 5 * * *", handleTokenRefresh);
+cron.schedule("0 9 * * *", handleTokenRefresh);
 // 7pm IST
 // cron.schedule("30 13 * * *", handleTokenRefresh);
 
 // cron.schedule("* * * * *", startListenData);
 
 // 10 pm IST
-cron.schedule("30 14 * * *", aggregateFundsData);
+// cron.schedule("30 14 * * *", aggregateFundsData);
